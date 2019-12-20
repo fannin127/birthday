f6c7784f86da9a1fd1a757b9e3513f5289dadd8c
Ryan Fannin <rf3g14@soton.ac.uk>
2019-12-20 19:36:38 +0000

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+
+import {SlideshowModule} from 'ng-simple-slideshow';
+import { AgmCoreModule } from '@agm/core';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlideshowModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDv7b0fuQqIGFQyIZHtVTKPGdlczGCKz3I'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
